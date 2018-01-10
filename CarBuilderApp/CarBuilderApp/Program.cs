f0d4902a262d0ece050fdedbc79d38708a4b125e
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-01-09 20:25:14 -0500

    Completed Car Builder App

--- CarBuilderApp/CarBuilderApp/Program.cs
+++ CarBuilderApp/CarBuilderApp/Program.cs
@@ -10,25 +10,59 @@ namespace CarBuilderApp
     {
         static void Main(string[] args)
         {
-            var car1 = new Car ("Red", "Toyota");
-            //car1.color = "Red";
-            //car1.manufacturer = "Toyota";
+            // instantiate car1 object and define it's color and manufacturer
+            var car1 = new Car
+            {
+                Color = "red",
+                Manufacturer = "Toyota"
+            };
 
-            Console.WriteLine("Car 1 is a " + car1.color + " " + car1.manufacturer);
+            Console.WriteLine("car1 is a " + car1.Color + " " + car1.Manufacturer);
+            Console.WriteLine();
 
+            // instantiate the three SaloonCar objects and define their properties based on the 3 constructors
+            var saloonCar1 = new SaloonCar (2);
+            var saloonCar2 = new SaloonCar (3, "Nissan");
+            var saloonCar3 = new SaloonCar (4, "purple", "Ford");
+
+            Console.WriteLine("salooncar1 is a car with " + saloonCar1.NumberOfSeats + " seats.");
+            Console.WriteLine("salooncar2 is a " + saloonCar2.Manufacturer + " with " + saloonCar2.NumberOfSeats + " seats.");
+            Console.WriteLine("salooncar3 is a " + saloonCar3.Color + " " + saloonCar3.Manufacturer + " with " + saloonCar3.NumberOfSeats + " seats.");
+            Console.WriteLine();
         }
     }
 
     public class Car
     {
-        public Car (string _color, string _manufacturer)
+        public Car ()
         {
-            this.color = _color;
-            this.manufacturer = _manufacturer;
         }
 
-        public string color { get; set; }
-        public string manufacturer { get; set; }
+        public string Color { get; set; }
+        public string Manufacturer { get; set; }
 
+    }
+
+    public class SaloonCar : Car
+    {
+        public SaloonCar (int _NumberOfSeats)
+        {
+            this.NumberOfSeats = _NumberOfSeats;
+        }
+
+        public SaloonCar (int _NumberOfSeats, string _manufacturer)
+        {
+            this.NumberOfSeats = _NumberOfSeats;
+            this.Manufacturer = _manufacturer;
+        }
+
+        public SaloonCar (int _NumberOfSeats, string _color, string _manufacturer)
+        {
+            this.NumberOfSeats = _NumberOfSeats;
+            this.Color = _color;
+            this.Manufacturer = _manufacturer;
+        }
+
+        public int NumberOfSeats { get; set; } 
     }
 }
