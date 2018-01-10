a06da6c901a0038ef1554adbf7456a0619508683
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-01-09 19:09:09 -0500

    Car class created. First car object instantiated

--- CarBuilderApp/CarBuilderApp/Program.cs
+++ CarBuilderApp/CarBuilderApp/Program.cs
@@ -10,8 +10,25 @@ namespace CarBuilderApp
     {
         static void Main(string[] args)
         {
+            var car1 = new Car ("Red", "Toyota");
+            //car1.color = "Red";
+            //car1.manufacturer = "Toyota";
+
+            Console.WriteLine("Car 1 is a " + car1.color + " " + car1.manufacturer);
+
         }
     }
 
     public class Car
+    {
+        public Car (string _color, string _manufacturer)
+        {
+            this.color = _color;
+            this.manufacturer = _manufacturer;
+        }
+
+        public string color { get; set; }
+        public string manufacturer { get; set; }
+
+    }
 }
